d8531cbc1aab0021cf413f07429081ac4b5f0270
ccristianss <cristian.silva9402@gmail.com>
2023-07-28 20:23:34 -0500

    230728 Fin Taller Consumo API's

--- TallerConsumoApi/static/js/detail.js
+++ TallerConsumoApi/static/js/detail.js
@@ -1,4 +1,44 @@
-var data = location.search;
-console.log(data);
-data = data.split("=");
-console.log(data[1]);
+let API = "https://rickandmortyapi.com/api/character";
+var query = location.search;
+var UriParams = new URLSearchParams(query);
+var id = UriParams.get("id");
+//console.log(id)
+fetch(API + "/" + id)
+  .then((res) => {
+    return res.json();
+  })
+  .then((data) => {
+    console.log(data);
+    var episodes = data.episode;
+    var episode = "";
+    for (let i = 0; i < episodes.length; i++) {
+      let e = episodes[i];
+      episode += `
+                      <h4>${e}</h4>
+          `;
+    }
+    var detail = `
+      <h1>${data.name}</h1>
+      <div class="imageDetail">
+        <img
+          src="${data.image}"
+          alt="${data.name}"
+        />
+      </div>
+
+      <div>
+        <h2>Status: ${data.status}</h2>
+        <h3>species: ${data.species}</h3>
+        <h3>gender: ${data.gender}</h3>
+        <h3>origin: ${data.origin.name}</h3>
+        <h3>location: ${data.location.name}</h3>
+      </div>
+      <div class="episodes">
+      ${episode}
+      </div>
+      <div>
+        <h3>${data.created}</h3>
+      </div>
+    `;
+    document.getElementById("detail").innerHTML = detail;
+  });
